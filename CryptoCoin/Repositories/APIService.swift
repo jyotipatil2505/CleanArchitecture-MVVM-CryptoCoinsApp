a7a745c17760a73930a329baa64c057aa999b22f
jyotipatil2505 <jyoti.mitkar55@gmail.com>
2024-12-10 16:35:11 +0530

    Removed print statements

--- CryptoCoin/Repositories/APIService.swift
+++ CryptoCoin/Repositories/APIService.swift
@@ -14,12 +14,8 @@ class APIService: APIServiceProtocol {
             let cryptoCoins: [CryptoCoinModel] = try await APIManager.shared.request(endpoint: endpoint)
             return cryptoCoins
         } catch let error as NetworkError {
-            // Handle specific network error
-            print("Network error: \(error)")
             throw error
         } catch {
-            // Handle other types of errors
-            print("Unknown error: \(error)")
             throw NetworkError.unknown(error)
         }
     }
